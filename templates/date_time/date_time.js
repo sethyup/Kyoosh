c48e6357a267ca9ae0fab2e2b2bf023d2fd2deb8
Adam Firdaus Tan <adambft98@gmail.com>
2022-10-19 00:11:38 +0800

    added date_time picker (still WIP)

---
+++ templates/date_time/date_time.js
@@ -0,0 +1,136 @@
+// // 1) API endpoint
+// let api_endpoint_url = "https://timezone.abstractapi.com/v1/current_time/?api_key=ffc998ad9ddc4166a4fcbbdb7da17aa3&location=Singapore"
+
+// // 2) Use Axios to call API asynchronously
+// axios.get(api_endpoint_url)
+// .then(response => {
+//     console.log(response.data)
+// })
+// .catch(error=> {
+//     // In case of any error, see what it's about
+//     console.log(error.message)
+// })
+
+var datetime_app = Vue.createApp({
+    data() {
+        return {
+            depart_datetime: "",
+            depart_tz: "",
+
+            arrive_datetime: "",
+            arrive_tz: "",
+
+            duration_hours: 0,
+            duration_minutes: 0,
+
+            user_timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+            curr_user_datetime: "",
+        }
+    },
+
+    methods: {
+        log_everything(thing) {
+            console.log("")
+            console.log("LOGGING EVERTHING ---------------")
+
+            console.log("depart_datetime:", this.depart_datetime)
+            console.log("depart_tz:", this.depart_tz)
+            console.log("arrive_datetime:", this.arrive_datetime)
+            console.log("arrive_tz:", this.arrive_tz)
+            console.log("duration_hours:", this.duration_hours)
+            console.log("duration_minutes:", this.duration_minutes)
+
+            console.log("LOGGED EVERYTHING----------------")
+            console.log("")
+        },
+
+        verify_hour() {
+            if (this.duration_hours == "") {
+                this.duration_hours = 0
+            } else {
+                this.duration_hours = this.duration_hours.toFixed()
+            }
+
+            if (this.duration_hours < 0) {
+                this.duration_hours = 0
+            }
+        },
+
+        verify_minite() {
+            if (this.duration_minutes == "") {
+                this.duration_minutes = 0
+            } else {
+                this.duration_minutes = this.duration_minutes.toFixed()
+            }
+
+            if (this.duration_minutes < 0) {
+                this.duration_minutes = 0
+            } else if (this.duration_minutes >= 60) {
+                this.duration_minutes = 59
+            }
+        },
+
+        update_datetimes(caller_event) {
+            let elem_calling = caller_event.path[0].getAttribute("datetime_obj")
+            console.log(elem_calling)
+
+            if (elem_calling == "duration-flight") {
+                //GOAL: Update arrival datetime
+                
+                //fill in fields that are not filled ----------------------------------------
+                if (this.depart_datetime == "") {
+                    console.log("setting depart datetime")
+                    this.depart_datetime = this.curr_user_datetime
+                    console.log(this.depart_datetime)
+                }
+                
+                if (this.depart_tz == "") {
+                    this.depart_tz = this.user_timezone
+                }
+
+                // initialize variables ----------------------------------------------------
+                let depart_time = this.depart_datetime
+                let depart_tz = this.depart_tz
+                let arrive_tz = this.arrive_tz
+                let duration_minutes_total = Number(this.duration_hours*60) + Number(this.duration_minutes)
+
+                // create depart and arrive Date objects -----------------------------------
+                // format: 2022-10-05 12:00
+                let arr_depart_datetime = depart_time.split(" ")
+                let datetime_date_arr = arr_depart_datetime[0].split("-")
+                let datetime_h_min_arr = arr_depart_datetime[1].split(":")
+
+                let depart_date_obj = new Date(datetime_date_arr[0], datetime_date_arr[1], datetime_date_arr[2], datetime_h_min_arr[0], datetime_h_min_arr[1])
+
+                let arrive_date_obj = new Date(depart_date_obj.getTime() + duration_minutes_total*60000)
+
+                console.log(depart_date_obj)
+                console.log(arrive_date_obj)
+            }
+        }
+    },
+
+    created() {
+        console.log("CREATING ---------------------------")
+        const time_elapsed = Date.now()
+        let current_date = new Date(time_elapsed)
+
+        let curr_datetime_temp = `${current_date.getFullYear()}-${("0" + (current_date.getMonth() + 1)).slice(-2)}-${("0" + (current_date.getDate())).slice(-2)} ${("0" + (current_date.getHours())).slice(-2)}:${("0" + (current_date.getMinutes())).slice(-2)}`
+
+        this.curr_user_datetime = curr_datetime_temp
+        console.log(this.curr_user_datetime)
+        console.log("DONE CREATING ----------------------")
+    }
+})
+
+datetime_app.mount('#datetime_app')
+
+
+// functions for buttons ------------------------------------------------------
+function get_val() {
+    console.log(document.getElementById("datetime_test").value)
+}
+
+function get_date() {
+    console.log(document.getElementById("date_test").value)
+}
