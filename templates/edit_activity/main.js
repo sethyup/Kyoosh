cf98b417c0bfca6c70d55cbee7dc66f630ebdb11
poojunsui <junsui.poo.2021@scis.smu.edu.sg>
2022-10-20 11:28:33 +0800

    create, edit activity templates

---
+++ templates/edit_activity/main.js
@@ -0,0 +1,48 @@
+const main = Vue.createApp({
+    data(){
+      return{
+        amount: "", 
+        from: "SGD", 
+        to: "KRW", 
+        converted_amount: "",
+        api_key: "wjnJhKhIK8qWrTVQ2YILd5wpxuyRGSP2",
+
+      }
+    },
+    methods:{
+        calculate_to_from() {
+            let api_endpoint_url = `https://api.apilayer.com/exchangerates_data/convert?to=${this.to}&from=${this.from}&amount=${this.amount}&apikey=${this.api_key}`
+            // 250 times per month 
+            
+            axios.get(api_endpoint_url)
+            .then(response => {
+                
+                // Inspect the response.data
+                let converted = response.data["result"]; 
+                this.converted_amount = (Math.round(converted*100))/100
+                
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+        },
+        calculate_from_to() {
+            let api_endpoint_url = `https://api.apilayer.com/exchangerates_data/convert?to=${this.from}&from=${this.to}&amount=${this.converted_amount}&apikey=${this.api_key}`
+            
+            axios.get(api_endpoint_url)
+            .then(response => {
+                
+                // Inspect the response.data
+                let converted = response.data["result"]; 
+                this.amount = (Math.round(converted*100))/100
+                
+            })
+            .catch(error => {
+                console.log(error.message)
+            })
+        }
+
+    }
+  })
+
+main.mount("#main")
