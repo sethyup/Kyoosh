88da7ee7439745a6f902d63ffd31a44d7c6665a2
FIRST_NAME LAST_NAME <wxseow.2021@scis.smu.edu.sg>
2022-10-31 03:05:53 +0800

    s

--- templates/pages/sidebars.js
+++ templates/pages/sidebars.js
@@ -128,7 +128,7 @@ sidebar.component('sidebar-phase2', {
 
                             <ul class="collapse nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
                                 <li class="w-100">
-                                    <a href="../../map.html" class="nav-link px-0"> 
+                                    <a href="../../map_phase2.html" class="nav-link px-0"> 
                                         <span class="d-none d-sm-inline">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-return-right" viewBox="0 0 16 16">
                                                 <path fill-rule="evenodd" d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5z"/>
