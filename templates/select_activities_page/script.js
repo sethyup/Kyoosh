fe38b18db601ce6e51c92e57573c44c3cdf82e2b
poojunsui <junsui.poo.2021@scis.smu.edu.sg>
2022-11-07 16:31:49 +0800

    select activities footer

--- templates/select_activities_page/script.js
+++ templates/select_activities_page/script.js
@@ -2,88 +2,105 @@ const main = Vue.createApp({
     data(){
         return{
             selected_all: false,
-            locations: [
-                {
-                    name: 'Seoul Forest Park, Ttukseom-ro, Seongdong-gu, Seoul, South Korea',
-                    address: '273 Ttukseom-ro, Seongdong-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5444, lng: 127.0374 },
-                    tag: 'Attraction',
-                    description: '',
-                    price: {sgd: 0, krw: 0},
-                    votes_percentage: {yes: 60, no: 20, yet_to_vote: 20}, 
-                    votes_num: {yes: 3, no: 1, yet_to_vote:1},
-                },
-                {
-                    name: 'Seoul Tower, Namsangongwon-gil, Yongsan-gu, Seoul, South Korea',
-                    address: '105 Namsangongwon-gil, Yongsan-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5512, lng: 126.9882 },
-                    tag: 'Attraction',
-                    description: '',
-                    price: {sgd: 12, krw: 12000},
-                    votes_percentage: {yes: 20, no: 20, yet_to_vote: 60}, 
-                    votes_num: {yes: 1, no: 1, yet_to_vote:3},
-                },
-                {
-                    name: 'Seoul Station Square, Hangang-daero, Jung-gu, Seoul, South Korea',
-                    address: '405 Hangang-daero, Jung-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5561, lng: 126.9719 },
-                    tag: 'Food',
-                    description: '',
-                    price: {sgd: 3, krw: 3000},
-                    votes_percentage: {yes: 80, no: 0, yet_to_vote: 20}, 
-                    votes_num: {yes: 4, no: 0, yet_to_vote:1},
-                },
-                {
-                    name: 'Hongdae Shopping Street',
-                    address: '365-8 Seogyo-dong, Mapo-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5532, lng: 126.9219 },
-                    tag: 'Shopping',
-                    description: '',
-                    price: {sgd: 25, krw: 25244.25}, 
-                    votes_percentage: {yes: 100, no: 0, yet_to_vote: 0}, 
-                    votes_num: {yes: 5, no: 0, yet_to_vote:0},
-                },
-                {
-                    name: 'Seoul Forest Park, Ttukseom-ro, Seongdong-gu, Seoul, South Korea',
-                    address: '273 Ttukseom-ro, Seongdong-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5444, lng: 127.0374 },
-                    tag: 'Attraction',
-                    description: '',
-                    price: {sgd: 0, krw: 0},
-                    votes_percentage: {yes: 60, no: 20, yet_to_vote: 20}, 
-                    votes_num: {yes: 3, no: 1, yet_to_vote:1},
-                },
-                {
-                    name: 'Seoul Tower, Namsangongwon-gil, Yongsan-gu, Seoul, South Korea',
-                    address: '105 Namsangongwon-gil, Yongsan-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5512, lng: 126.9882 },
-                    tag: 'Attraction',
-                    description: '',
-                    price: {sgd: 12, krw: 12000},
-                    votes_percentage: {yes: 20, no: 20, yet_to_vote: 60}, 
-                    votes_num: {yes: 1, no: 1, yet_to_vote:3},
-                },
-                {
-                    name: 'Seoul Station Square, Hangang-daero, Jung-gu, Seoul, South Korea',
-                    address: '405 Hangang-daero, Jung-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5561, lng: 126.9719 },
-                    tag: 'Food',
-                    description: '',
-                    price: {sgd: 3, krw: 3000},
-                    votes_percentage: {yes: 80, no: 0, yet_to_vote: 20}, 
-                    votes_num: {yes: 4, no: 0, yet_to_vote:1},
-                },
-                {
-                    name: 'Hongdae Shopping Street',
-                    address: '365-8 Seogyo-dong, Mapo-gu, Seoul, South Korea',
-                    latlng: { lat: 37.5532, lng: 126.9219 },
-                    tag: 'Shopping',
-                    description: '',
-                    price: {sgd: 25, krw: 25244.25}, 
-                    votes_percentage: {yes: 100, no: 0, yet_to_vote: 0}, 
-                    votes_num: {yes: 5, no: 0, yet_to_vote:0},
-                },
-                    ],
+            locations  : { 
+                "1": {
+                    "address": "273 Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
+                    "description": "",
+                    "latlng": {
+                        "lat": 37.5444,
+                        "lng": 127.0374
+                    },
+                    "name": "Seoul Forest Park, Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
+                    "price": {
+                        "krw": 0,
+                        "sgd": 0
+                    },
+                    "tag": "Attraction",
+                    "votes_num": {
+                        "no": 1,
+                        "yes": 3,
+                        "yet_to_vote": 1
+                    },
+                    "votes_percentage": {
+                        "no": 20,
+                        "yes": 60,
+                        "yet_to_vote": 20
+                    }
+                    },
+                    "2":{
+                    "address": "105 Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
+                    "description": "",
+                    "latlng": {
+                        "lat": 37.5512,
+                        "lng": 126.9882
+                    },
+                    "name": "Seoul Tower, Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
+                    "price": {
+                        "krw": 12000,
+                        "sgd": 12
+                    },
+                    "tag": "Attraction",
+                    "votes_num": {
+                        "no": 1,
+                        "yes": 1,
+                        "yet_to_vote": 3
+                    },
+                    "votes_percentage": {
+                        "no": 20,
+                        "yes": 20,
+                        "yet_to_vote": 60
+                    }
+                    },
+                    "3":{
+                    "address": "405 Hangang-daero, Jung-gu, Seoul, South Korea",
+                    "description": "",
+                    "latlng": {
+                        "lat": 37.5561,
+                        "lng": 126.9719
+                    },
+                    "name": "Seoul Station Square, Hangang-daero, Jung-gu, Seoul, South Korea",
+                    "price": {
+                        "krw": 3000,
+                        "sgd": 3
+                    },
+                    "tag": "Food",
+                    "votes_num": {
+                        "no": 0,
+                        "yes": 4,
+                        "yet_to_vote": 1
+                    },
+                    "votes_percentage": {
+                        "no": 0,
+                        "yes": 80,
+                        "yet_to_vote": 20
+                    }
+                    },
+                    "4":{
+                    "address": "365-8 Seogyo-dong, Mapo-gu, Seoul, South Korea",
+                    "description": "",
+                    "latlng": {
+                        "lat": 37.5532,
+                        "lng": 126.9219
+                    },
+                    "name": "Hongdae Shopping Street",
+                    "price": {
+                        "krw": 25244.25,
+                        "sgd": 25
+                    },
+                    "tag": "Shopping",
+                    "votes_num": {
+                        "no": 0,
+                        "yes": 5,
+                        "yet_to_vote": 0
+                    },
+                    "votes_percentage": {
+                        "no": 0,
+                        "yes": 100,
+                        "yet_to_vote": 0
+                    }
+                }, 
+                
+            },
             total_price_list: [],
             spending: 0,
 
