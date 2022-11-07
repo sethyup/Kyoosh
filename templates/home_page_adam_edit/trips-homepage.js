2c75127f62109ef023e4709c21a1564b25b470a9
Adam Firdaus Tan <adambft98@gmail.com>
2022-11-08 00:56:25 +0800

    Created typing effect on trips-homepage (adam edit)

--- templates/home_page_adam_edit/trips-homepage.js
+++ templates/home_page_adam_edit/trips-homepage.js
@@ -1,19 +1,65 @@
-const homepage_app = Vue.createApp( {
+// List of sentences
+var _CONTENT = [ "Plan a trip.", "Find yourself.", "Get away.", "Live your dream." ];
 
-    //=========== DATA PROPERTIES ===========
-    data() {
-        return {
-            curr_img_id: 1,
-            num_images: 5,
-        }
-    },
+// Current sentence being processed
+var _PART = 0;
 
-    //=========== METHODS ===========
-    methods: {
+// Character number of the current sentence being processed 
+var _PART_INDEX = 0;
 
-        func_1() {
-        },
-    },
-})
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
 
-homepage_app.mount('#homepage_app')
+// Element that holds the text
+var _ELEMENT = document.querySelector("#animated-text");
+
+// How long to hold?
+var hold_main = 5000
+var hold_sub = 1500
+
+// Implements typing effect
+function Type() { 
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+        var to_hold
+        if (text == _CONTENT[0]) {
+            to_hold = hold_main
+        } else {to_hold = hold_sub}
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, to_hold);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If last sentence then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
