19107784ee0bb316c1471ecb6cb2d3cdcca65f72
poojunsui <junsui.poo.2021@scis.smu.edu.sg>
2022-11-09 20:43:16 +0800

    activity card update

--- templates/activity_card_listview/script.js
+++ templates/activity_card_listview/script.js
@@ -20,85 +20,85 @@ const main = Vue.createApp({
     data(){
       return{
         // retrieve from database
-        locations  : { 
-          "1": {
-              "address": "273 Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
-              "description": "",
-              "latlng": {
-                  "lat": 37.5444,
-                  "lng": 127.0374
-              },
-              "name": "Seoul Forest Park, Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
-              "price": {
-                  "krw": 0,
-                  "sgd": 0
-              },
-              "tag": "Attraction",
-              "votes": {
-                  yes: ["SETH YAP ZIQI_", "kbang"],
-                  no: ["adamft"],
-                  yet_to_vote: ["name4", "name5"]
-              }
-              },
-              "2":{
-              "address": "105 Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
-              "description": "",
-              "latlng": {
-                  "lat": 37.5512,
-                  "lng": 126.9882
-              },
-              "name": "Seoul Tower, Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
-              "price": {
-                  "krw": 12000,
-                  "sgd": 12
-              },
-              "tag": "Attraction",
-              "votes": {
-                yes: ["SETH YAP ZIQI_"],
-                no: ["adamft", "kbang" ],
-                yet_to_vote: ["name4", "name5"]
-              }
-              },
-              "3":{
-              "address": "405 Hangang-daero, Jung-gu, Seoul, South Korea",
-              "description": "",
-              "latlng": {
-                  "lat": 37.5561,
-                  "lng": 126.9719
-              },
-              "name": "Seoul Station Square, Hangang-daero, Jung-gu, Seoul, South Korea",
-              "price": {
-                  "krw": 3000,
-                  "sgd": 3
-              },
-              "tag": "Food",
-              "votes": {
-                yes: ["SETH YAP ZIQI_"],
-                no: ["adamft"],
-                yet_to_vote: ["kbang", "name4", "name5"]
-              }
-              },
-              "4":{
-              "address": "365-8 Seogyo-dong, Mapo-gu, Seoul, South Korea",
-              "description": "",
-              "latlng": {
-                  "lat": 37.5532,
-                  "lng": 126.9219
-              },
-              "name": "Hongdae Shopping Street",
-              "price": {
-                  "krw": 25244.25,
-                  "sgd": 25
-              },
-              "tag": "Shopping",
-              "votes": {
-                yes: ["SETH YAP ZIQI_"],
-                no: ["adamft", "name4"],
-                yet_to_vote: ["kbang", "name5"]
-              }
-          }, 
+      //   locations  : { 
+      //     "1": {
+      //         "address": "273 Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
+      //         "description": "",
+      //         "latlng": {
+      //             "lat": 37.5444,
+      //             "lng": 127.0374
+      //         },
+      //         "name": "Seoul Forest Park, Ttukseom-ro, Seongdong-gu, Seoul, South Korea",
+      //         "price": {
+      //             "krw": 0,
+      //             "sgd": 0
+      //         },
+      //         "tag": "Attraction",
+      //         "votes": {
+      //             yes: ["SETH YAP ZIQI_", "kbang"],
+      //             no: ["adamft"],
+      //             yet_to_vote: ["name4", "name5"]
+      //         }
+      //         },
+      //         "2":{
+      //         "address": "105 Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
+      //         "description": "",
+      //         "latlng": {
+      //             "lat": 37.5512,
+      //             "lng": 126.9882
+      //         },
+      //         "name": "Seoul Tower, Namsangongwon-gil, Yongsan-gu, Seoul, South Korea",
+      //         "price": {
+      //             "krw": 12000,
+      //             "sgd": 12
+      //         },
+      //         "tag": "Attraction",
+      //         "votes": {
+      //           yes: ["SETH YAP ZIQI_"],
+      //           no: ["adamft", "kbang" ],
+      //           yet_to_vote: ["name4", "name5"]
+      //         }
+      //         },
+      //         "3":{
+      //         "address": "405 Hangang-daero, Jung-gu, Seoul, South Korea",
+      //         "description": "",
+      //         "latlng": {
+      //             "lat": 37.5561,
+      //             "lng": 126.9719
+      //         },
+      //         "name": "Seoul Station Square, Hangang-daero, Jung-gu, Seoul, South Korea",
+      //         "price": {
+      //             "krw": 3000,
+      //             "sgd": 3
+      //         },
+      //         "tag": "Food",
+      //         "votes": {
+      //           yes: ["SETH YAP ZIQI_"],
+      //           no: ["adamft"],
+      //           yet_to_vote: ["kbang", "name4", "name5"]
+      //         }
+      //         },
+      //         "4":{
+      //         "address": "365-8 Seogyo-dong, Mapo-gu, Seoul, South Korea",
+      //         "description": "",
+      //         "latlng": {
+      //             "lat": 37.5532,
+      //             "lng": 126.9219
+      //         },
+      //         "name": "Hongdae Shopping Street",
+      //         "price": {
+      //             "krw": 25244.25,
+      //             "sgd": 25
+      //         },
+      //         "tag": "Shopping",
+      //         "votes": {
+      //           yes: ["SETH YAP ZIQI_"],
+      //           no: ["adamft", "name4"],
+      //           yet_to_vote: ["kbang", "name5"]
+      //         }
+      //     }, 
           
-      },
+      // },
         //HARD-CODED
         user_name: "kbang",
         trip_id: "grad trip_adambft",
@@ -107,35 +107,58 @@ const main = Vue.createApp({
     },
     methods:{
         get_total_users(){
-          var all_votes = this.locations["1"].votes
-          console.log(`${all_votes} this from get total users`)
-          var total_users = all_votes.yes.length + all_votes.no.length + all_votes.yet_to_vote.length
-          console.log(this.get_total_users)
+          var total_users = 0
+          var votes = this.existing_locations[0].votes
+          // console.log(`${all_votes} this from get total users`)
+          if(votes.yes){
+            total_users += votes.yes.length
+          }
+          if(votes.no){
+            total_users += votes.no.length
+          }
+          if(votes.yet_to_vote){
+            total_users += votes.yet_to_vote.length
+          }
+          // var total_users = all_votes.yes.length + all_votes.no.length + all_votes.yet_to_vote.length
+          // console.log(this.get_total_users)
           return total_users
         },
         get_yes_num(votes){
-          console.log(typeof String(votes.yes.length))
-          return String(votes.yes.length)
+          // console.log(typeof String(votes.yes.length))
+          var yes_votes = 0 
+          if(votes.yes){
+            yes_votes += votes.yes.length
+          }
+          return yes_votes
         },
         get_no_num(votes){
-          // console.log(votes.no.length)
-          return String(votes.no.length)
+          var no_votes = 0 
+          if(votes.no){
+            no_votes += votes.no.length
+          }
+          return no_votes
         },
         get_yet_to_vote_num(votes){
-          // console.log(votes.yet_to_vote.length)
-          return String(votes.yet_to_vote.length)
+          var yet_to_vote_votes = 0 
+          if(votes.yet_to_vote){
+            yet_to_vote_votes += votes.yet_to_vote.length
+          }
+          return yet_to_vote_votes
         },
         get_yes_percentage(votes){
           // console.log(votes.yes.length)
-          return (votes.yes.length)*100/this.get_total_users()
+          var yes_votes = this.get_yes_num(votes)
+          return (yes_votes)*100/this.get_total_users()
         },
         get_no_percentage(votes){
           // console.log(votes.no.length)
-          return (votes.no.length)*100/this.get_total_users()
+          var no_votes = this.get_no_num(votes)
+          return (no_votes)*100/this.get_total_users()
         },
         get_yet_to_vote_percentage(votes){
           // console.log(votes.yet_to_vote.length)
-          return (votes.yet_to_vote.length)*100/this.get_total_users()
+          var yet_to_vote_votes = this.get_yet_to_vote_num(votes)
+          return (yet_to_vote_votes)*100/this.get_total_users()
         },
         user_reject(votes, idx){
             // number of members = 5
@@ -176,28 +199,28 @@ const main = Vue.createApp({
             votes.yes.push(this.user_name)
             votes.yet_to_vote.splice(votes.yes.indexOf(this.user_name),1)
           }
-        }
-            // update tooltip
-            var tooltip_no = bootstrap.Tooltip.getInstance('#progress_no'+ idx);
-            tooltip_no.setContent({ '.tooltip-inner': String(votes.no.length) });
-            console.log("#progress_no" + idx)
-            var tooltip_yes = bootstrap.Tooltip.getInstance('#progress_yes'+ idx);
-            tooltip_yes.setContent({ '.tooltip-inner': String(votes.yes.length) });
+        }},
+        //     // update tooltip
+        //     var tooltip_no = bootstrap.Tooltip.getInstance('#progress_no'+ idx);
+        //     tooltip_no.setContent({ '.tooltip-inner': String(votes.no.length) });
+        //     console.log("#progress_no" + idx)
+        //     var tooltip_yes = bootstrap.Tooltip.getInstance('#progress_yes'+ idx);
+        //     tooltip_yes.setContent({ '.tooltip-inner': String(votes.yes.length) });
 
-            var tooltip_maybe = bootstrap.Tooltip.getInstance('#progress_maybe'+ idx);
-            tooltip_maybe.setContent({ '.tooltip-inner': String(votes.yet_to_vote.length)});
-        }, 
+        //     var tooltip_maybe = bootstrap.Tooltip.getInstance('#progress_maybe'+ idx);
+        //     tooltip_maybe.setContent({ '.tooltip-inner': String(votes.yet_to_vote.length)});
+        // }, 
         // read existing locations from the database
         read_from_existing_locations() {
           const data_to_be_read = ref(db, `trips/${this.trip_id}/activities`);
           onValue(data_to_be_read, (snapshot) => {
               const data = snapshot.val();
-              // check if there is existing data on db
+              //check if there is existing data on db
               if (data) {
                   this.existing_locations = data
                   console.log(data)
               }
-              // retrieve recommended places for new trips
+              //retrieve recommended places for new trips
               else {
                   const data_to_be_read = ref(db, `locations`);
                   onValue(data_to_be_read, (snapshot) => {
@@ -209,6 +232,40 @@ const main = Vue.createApp({
               }
               })
         },
+
+        create_update_data(votes, indx) {
+          console.log("Writing data into database...")
+          // the console can be open, 
+
+          // Database path must be set by you
+          // e.g. users/junsui/friends
+
+          // EDIT HERE
+          set(ref(db, /* PATH GOES HERE */), {
+              // DATA YOU WANT TO WRITE GOES HERE,
+
+              // example
+              // email: this.email
+              // ...
+
+            })
+          .then(
+              function write_success() {
+                  // display "Success" message
+                  alert("Write Operation Successful")
+                  console.log("Entry Created")
+          })
+          .catch((error) => {
+              // for us to debug, tells us what error there is,
+              const errorCode = error.code;
+              const errorMessage = error.message;
+
+              // display "Error" message
+              var failed_message = `Write Operation Unsuccessful. Error Code ${errorCode}: ${errorMessage}`
+              alert(failed_message)
+              console.log(failed_message);
+          })
+      },
     },
     async created() {
       await this.read_from_existing_locations()
