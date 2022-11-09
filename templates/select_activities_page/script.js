90bf29a943d1c012867fe715625f3de098870283
poojunsui <junsui.poo.2021@scis.smu.edu.sg>
2022-11-09 17:35:03 +0800

    select activities page

--- templates/select_activities_page/script.js
+++ templates/select_activities_page/script.js
@@ -192,29 +192,30 @@ const main = Vue.createApp({
                 // this.spending += Number(this.selected_activities[i])
             };
         }, 
-        compareValues(key, order = 'asc') {
-            return function innerSort(a, b) {
-              if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-                // property doesn't exist on either object
-                return 0;
-              }
+        // compareValues(key, order = 'asc') {
+        //     return function innerSort(a, b) {
+        //       if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        //         // property doesn't exist on either object
+        //         return 0;
+        //       }
           
-              const varA = (typeof a[key] === 'string')
-                ? a[key].toUpperCase() : a[key];
-              const varB = (typeof b[key] === 'string')
-                ? b[key].toUpperCase() : b[key];
+        //       const varA = (typeof a[key] === 'string')
+        //         ? a[key].toUpperCase() : a[key];
+        //       const varB = (typeof b[key] === 'string')
+        //         ? b[key].toUpperCase() : b[key];
           
-              let comparison = 0;
-              if (varA > varB) {
-                comparison = 1;
-              } else if (varA < varB) {
-                comparison = -1;
-              }
-              return (
-                (order === 'desc') ? (comparison * -1) : comparison
-              );
-            };
-        }
+        //       let comparison = 0;
+        //       if (varA > varB) {
+        //         comparison = 1;
+        //       } else if (varA < varB) {
+        //         comparison = -1;
+        //       }
+        //       return (
+        //         (order === 'desc') ? (comparison * -1) : comparison
+        //       );
+        //     };
+        //     // console.log(this.calculate_list[1])
+        // }
         
     }
 })
