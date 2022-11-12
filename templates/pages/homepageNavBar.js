519726cd5ced224da98068d326ed6e22f9935af9
Kay Bee <77047528+kengboonang@users.noreply.github.com>
2022-11-13 05:48:44 +0800

    Revert "Merge branch 'main' of edit_copy.js"
    This reverts commit 6726522b8238c6bf1e82e02928c3ef5778c4ddd7, reversing
    changes made to 1b629780b32a36e00ac9339c7d8a9eeefe0a53e2.

--- templates/pages/homepageNavBar.js
+++ templates/pages/homepageNavBar.js
@@ -6,7 +6,7 @@ main.component('homepage-navbar', {
     template: 
             `<nav class="navbar navbar-expand-sm navbar-light border-bottom sticky-top blurred">
                 <div class="container">
-                    <a class="navbar-brand" href="#"><b>Kyoosh</b></a>
+                    <a class="navbar-brand" href="#"><b>The JAWKS-7</b></a>
                         <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
                             aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
@@ -17,56 +17,64 @@ main.component('homepage-navbar', {
                             <li class="nav-item">
                                 <button type="button" class="d-sm-none nav-link btn btn-main-fixed" style="width: 150px;" onclick="location.href='create-trip.html' ">+ Plan New Trip</button>                       
                             </li>
+                            <li class="nav-item">
+                                <a class="nav-link active" href="trips-homepage.html" aria-current="page">Trips <span class="visually-hidden">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Friends</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="d-sm-none nav-link" href="#">Account</a>
+                            </li> 
                         </ul>
 
                         <div style="width: calc(8vw + 150px)" class="d-flex justify-content-center">
                             <button type="button" class="d-none d-sm-block btn btn-main mx-3" onclick="location.href='create-trip.html'">+ Plan New Trip</button>
                         </div>
                         
-                        <div class="dropdown">
-                            <a href="#" class="d-flex align-items-center text-success text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Mr._Bean_2011.jpg" alt="profilepic" width="30" height="30" class="rounded-circle">
-                                <span class="d-none d-sm-inline mx-1">Bean</span>
-                            </a>
-                            <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                                <li><a class="dropdown-item" href="https://kengboonang.github.io/WADBrothers.github.io/templates/pages/signup_login%20pages/login_page.html">Sign out</a></li>
-                            </ul>
-                        </div>
+                        <!-- <a class="d-none d-sm-block" href="#">
+                            <img width="40px" src="https://img.icons8.com/ios-filled/100/000000/user-male-circle.png"/>
+                        </a> -->
+
+                        <a class="d-none d-sm-block" href="#">
+                            <i class="fa-solid fa-circle-user fa-2xl text-dark"></i>
+                        </a>
                     </div>
                     
                 </div>
-            </nav>
-            `
-})
+            </nav>`
+    })
 
 main.component('createtrip-navbar', {
     template: 
             `<nav class="navbar navbar-expand-sm navbar-light border-bottom sticky-top blurred">
-            <div class="container">
-                <a class="navbar-brand" href="#"><b>Kyoosh</b></a>
-                <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
-                    aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
+                <div class="container">
+                    <a class="navbar-brand" href="#"><b>The JAWKS-7</b></a>
+                        <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
 
-                <div class="collapse navbar-collapse justify-content-end" id="collapsibleNavId">
-                    <div class="dropdown d-none d-sm-inline">
-                        <a href="#" class="text-dark text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/3/33/Mr._Bean_2011.jpg" alt="profilepic" width="30" height="30" class="rounded-circle">
-                            <span class="d-sm-inline mx-1">Bean</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
-                            <li><a class="dropdown-item" href="https://kengboonang.github.io/WADBrothers.github.io/templates/pages/signup_login%20pages/login_page.html">Sign out</a></li>
+                    <div class="collapse navbar-collapse" id="collapsibleNavId">        
+                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" href="trips-homepage.html" aria-current="page">Trips <span class="visually-hidden">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Friends</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="d-sm-none nav-link" href="#">Account</a>
+                            </li> 
                         </ul>
+            
+                        <a class="d-none d-sm-block" href="#">
+                            <i class="fa-solid fa-circle-user fa-2xl text-dark"></i>
+                        </a>
                     </div>
 
-                    <div class="dropdown d-sm-none">
-                        <button type="button" class="btn btn-outline-warning my-2" onclick="location.href='https://kengboonang.github.io/WADBrothers.github.io/templates/pages/signup_login%20pages/login_page.html'">Sign Out</button>
-                    </div>
                 </div>
-                
-            </div>
-        </nav>`
+            </nav>`
     })
 
 main.mount('#nav')
--- templates/vacation_view/vacation_view.js
+++ templates/vacation_view/vacation_view.js
@@ -9,27 +9,6 @@ if (localStorage.getItem("user") === null) {
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getDatabase, ref, onValue, get, push, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
 
-// DATETIME FUNCTIONS
-function convert_date_obj_to_str(date_obj) {
-    console.log("")
-    console.log("CALLING convert_date_obj_to_str() ---------------")
-
-    console.log("date_obj (input):", date_obj)
-
-    return `${date_obj.getFullYear()}-${("0" + (date_obj.getMonth()+1)).slice(-2)}-${("0" + (date_obj.getDate())).slice(-2)} ${("0" + (date_obj.getHours())).slice(-2)}:${("0" + (date_obj.getMinutes())).slice(-2)}`
-}
-
-function convert_datetime_str_to_date_obj(datetime_str) {
-    // format: 2022-10-05 12:00
-    let arr_depart_datetime = datetime_str.split(" ")
-    let datetime_date_arr = arr_depart_datetime[0].split("-")
-    let datetime_h_min_arr = arr_depart_datetime[1].split(":")
-
-    let new_date_obj = new Date(datetime_date_arr[0], Number(datetime_date_arr[1])-1, datetime_date_arr[2], datetime_h_min_arr[0], datetime_h_min_arr[1])
-
-    return new_date_obj
-}
-
 // Our Firebase Project Configuration
 const WADTravel = initializeApp({
   apiKey: "AIzaSyCR5RtPZexqY6jCbDZsaYzyUpVE_q8vzMc",
@@ -53,6 +32,7 @@ const main = Vue.createApp({
             date_array: [],
             flights_details: "", 
             lodging_details: "",
+
         }
     },
     computed:{
@@ -90,7 +70,6 @@ const main = Vue.createApp({
                 // }
                 })
         },
-        
         // read flight stuff from db
         read_flight_details() {
             const data_to_be_read = ref(db, `trips/${this.trip_id}/flights`);
@@ -145,13 +124,11 @@ const main = Vue.createApp({
                     // console.log(s_date, e_date)
                     this.create_dates_array(s_date, e_date)
                 }
-            })
-        },
-
+                })
+            },
         convert_date_obj_to_str(date_obj) {
-            return `${date_obj.getFullYear()}-${("0" + (date_obj.getMonth()+1)).slice(-2)}-${("0" + (date_obj.getDate())).slice(-2)}`
-        },
-
+                return `${date_obj.getFullYear()}-${("0" + (date_obj.getMonth()+1)).slice(-2)}-${("0" + (date_obj.getDate())).slice(-2)}`
+            },
         create_dates_array(s_date, e_date){
                 var s_date_obj = new Date(s_date)
                 var e_date_obj = new Date(e_date)
@@ -168,24 +145,9 @@ const main = Vue.createApp({
             console.log("DATES ARRAY:", this.date_array)
             
                 // return this.date_array
-        },
+            },
 
-        // DATETIME METHODS
-        convert_datetime_to_readable(datetime_str) {
-            var date_obj = convert_datetime_str_to_date_obj(datetime_str)
 
-            return flatpickr.formatDate(date_obj, "h:i K, j F Y (D)")
-        },
-
-        convert_datetime_to_readable_time(datetime_str) {
-            var date_obj = convert_datetime_str_to_date_obj(datetime_str)
-
-            return flatpickr.formatDate(date_obj, "h:i K")
-        },
-
-        convert_date_to_readable(date_str) {
-            return flatpickr.formatDate(new Date(date_str), "j F (D)")
-        }
         
     },
     // retrieve data from database onload
