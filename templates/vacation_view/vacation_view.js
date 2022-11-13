cc2f5ccf12dac5877f5ba52b990c0ad0443edb4b
Adam Firdaus Tan <adambft98@gmail.com>
2022-11-13 13:07:08 +0800

    styling edited on vacation view

--- templates/vacation_view/vacation_view.js
+++ templates/vacation_view/vacation_view.js
@@ -9,6 +9,9 @@ if (localStorage.getItem("user") === null) {
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-app.js";
 import { getDatabase, ref, onValue, get, push, set } from "https://www.gstatic.com/firebasejs/9.12.1/firebase-database.js";
 
+// list of months
+const month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+
 // DATETIME FUNCTIONS
 function convert_date_obj_to_str(date_obj) {
     console.log("")
@@ -53,6 +56,9 @@ const main = Vue.createApp({
             date_array: [],
             flights_details: "", 
             lodging_details: "",
+
+            user_id: "",
+            trip_details: {},
         }
     },
     computed:{
@@ -185,12 +191,50 @@ const main = Vue.createApp({
 
         convert_date_to_readable(date_str) {
             return flatpickr.formatDate(new Date(date_str), "j F (D)")
-        }
+        },
+
+        // Datetime details
+        convert_datetime_str_to_date_obj(datetime_str) {
+            // format: 2022-10-05
+            let arr_depart_datetime = datetime_str.split(" ")
+            let datetime_date_arr = arr_depart_datetime[0].split("-")
+
+            let new_date_obj = new Date(datetime_date_arr[0], Number(datetime_date_arr[1])-1, datetime_date_arr[2])
+
+            return new_date_obj
+        },
+
+        // retrieve trip details from localStorage
+        retrieve_from_cache() {
+            if (localStorage.getItem('user')) {
+                this.user_id = localStorage.getItem('user')
+            }
+            if (localStorage.getItem('trip')) {
+                this.trip_id = localStorage.getItem('trip')
+            }
+            if (localStorage.getItem('trip_start_date')) {
+                // format YYYY-MM-DD to "DD Month Year"
+                var start_date = this.convert_datetime_str_to_date_obj(localStorage.getItem('trip_start_date'))
+                var end_date = this.convert_datetime_str_to_date_obj(localStorage.getItem('trip_end_date'))
+                // set duration
+                var c_duration = `${start_date.getDate()} ${month[start_date.getMonth()]} ${start_date.getFullYear()} - ${end_date.getDate()} ${month[end_date.getMonth()]} ${end_date.getFullYear()}`
+                this.trip_details.duration = c_duration
+            }
+            if (localStorage.getItem('destination')) {
+                var c_country = localStorage.getItem('destination')
+                this.trip_details.country = c_country
+            }
+        },
+
+        // GET TRIP NAME
+        get_trip_name(tripID) {
+            return tripID.split("urjfjwowskdorrofkckshecoejfnek")[0]
+        },
         
     },
     // retrieve data from database onload
     async created() {
-        this.trip_id = localStorage.getItem("trip")
+        this.retrieve_from_cache()
 
         await this.read_itinerary()
         await this.read_flight_details()
