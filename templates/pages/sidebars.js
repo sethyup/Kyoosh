e960dfdbd19efea2620048ef342a9f3775a530b5
Adam Firdaus Tan <adambft98@gmail.com>
2022-11-14 16:41:50 +0800

    Edited position of error message on login & removed alerts from log out

--- templates/pages/sidebars.js
+++ templates/pages/sidebars.js
@@ -57,12 +57,11 @@ const sidebar = Vue.createApp({
             console.log("starting to log out user...")
             signOut(auth).then(
             function success_sign_out() {
-                alert("sign out successful")
                 console.log("sign out successful")
                 localStorage.clear()
             },
             function failed_sign_out() {
-                alert("sign out failed")
+                alert("Sign out failed")
                 console.log("sign out failed")
             }
             )
